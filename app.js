8fbfba99d80db746572d625b64c22cbfbbdc55bc
Mo Shawa <moshawa0@gmail.com>
2021-11-01 18:50:42 -0400

    fix duplicates bug

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
 const pHandEl = document.querySelector(".pHand");
 const dHandEl = document.querySelector(".dHand");
 const deckEl = document.querySelector(".deck");
+const textEl = document.querySelector(".text");
 //buttons
 const hitBtn = document.querySelector(".hit");
 const stayBtn = document.querySelector(".stay");
@@ -23,156 +24,161 @@ let dHandVal = 0;
 
 //FUNCTIONS
 function init() {
-  pHand = [];
-  pHandVal = 0;
-  dHand = [];
-  dHandVal = 0;
-  end = false;
-  deck = [];
-  makeDeck(deck);
-  shuffle(deck);
-  deal(deck);
-  render();
+	pHand = [];
+	pHandVal = 0;
+	dHand = [];
+	dHandVal = 0;
+	end = false;
+	deck = [];
+	makeDeck(deck);
+	shuffle(deck);
+	deal(deck);
+	render();
 }
 
 //Render
 function render() {
-  pHandVal = calcHandVal(pHand);
-  // if i disable either textContent, the whole thing breaks???
-  pHandEl.textContent = pHandVal
+	pHandVal = calcHandVal(pHand);
+	// if i disable either textContent, the whole thing breaks??
+	// turns out it was clearing the html, if you take that out you get duplicates
+	pHandEl.innerHTML = "";
 
-  dHandVal = calcHandVal(dHand);
-  dHandEl.textContent = dHandVal 
-  calcHandVal(dHand);
+	dHandVal = calcHandVal(dHand);
+	dHandEl.innerHTML = "";
+	calcHandVal(dHand);
 
-  checkBust()
+	checkBust();
 
-  // render dealt cards
-  
-  // render player hand
-  pHand.forEach(function (card) {
-    let cd = document.createElement("div");
-    cd.classList.add("card");
-    
-    // Account for css class naming format
-    let parseVal;
-    typeof card.value == "number" && card.value < 10
-      ? (parseVal = `0${card.value}`)
-      : (parseVal = card.value);
-    cd.classList.add(card.suit[0] + parseVal);
-	
-    pHandEl.appendChild(cd);
-  });
+	// render dealt cards
 
-  // render dealer hand
-  dHand.forEach(function (card) {
-    let cd = document.createElement("div");
-    cd.classList.add("card");
-    // console.log(card.suit, card.value);
-    // Account for css class naming format
-    let parseVal;
-    typeof card.value == "number" && card.value < 10
-      ? (parseVal = `0${card.value}`)
-      : (parseVal = card.value);
-    cd.classList.add(card.suit[0] + parseVal);
+	// render player hand
+	pHand.forEach(function (card) {
+		let cd = document.createElement("div");
+		cd.classList.add("card");
 
-    // console.log(cd);
-    dHandEl.appendChild(cd);
-  });
+		// Account for css class naming format
+		let parseVal;
+		typeof card.value == "number" && card.value < 10
+			? (parseVal = `0${card.value}`)
+			: (parseVal = card.value);
+		cd.classList.add(card.suit[0] + parseVal);
+
+		pHandEl.appendChild(cd);
+	});
+
+	// render dealer hand
+	// probably could have avoided this by creating user and dealer objects
+	dHand.forEach(function (card) {
+		let cd = document.createElement("div");
+		cd.classList.add("card");
+		// console.log(card.suit, card.value);
+		// Account for css class naming format
+		let parseVal;
+		typeof card.value == "number" && card.value < 10
+			? (parseVal = `0${card.value}`)
+			: (parseVal = card.value);
+		cd.classList.add(card.suit[0] + parseVal);
+
+		// console.log(cd);
+		dHandEl.appendChild(cd);
+	});
 }
 
-// hit function 
-function hit(){
-  if(checkBust()){
-    return
-  } else{
-  pHand.push(deck.pop())}  
-  render()
+// hit function
+function hit() {
+	if (checkBust()) {
+		console.log("House wins! Press Deal to play again");
+		return;
+	} else {
+		pHand.push(deck.pop());
+	}
+	render();
 }
 
 // stay function
-function stay(){
-  console.log('stey')
+function stay() {
+	checkDealer();
+	console.log("stey");
 }
 
-// check bust 
+// check dealer
+function checkDealer() {
+	dHandVal < 17 ? dHand.push(deck.pop()) : undefined;
+}
 
-function checkBust(){
-  if (pHandVal > 21){
-    pHandEl.style.color = 'red'
-    return true
-
-  }else {
-    return
-    false
-  }
+// check bust
+function checkBust() {
+	return pHandVal > 21;
+	// {
+	// 	pHandEl.style.color = "red";
+	// 	return true;
+	// } else {
+	// 	return false;
+	// }
 }
 
 //Create a deck
 
 function makeDeck(deck) {
-  suits.forEach(function (suit) {
-    values.forEach(function (value) {
-      let card = {};
-      card.suit = suit;
-      card.value = value;
-      deck.push(card);
-    });
-  });
-  return deck;
+	suits.forEach(function (suit) {
+		values.forEach(function (value) {
+			let card = {};
+			card.suit = suit;
+			card.value = value;
+			deck.push(card);
+		});
+	});
+	return deck;
 }
 
 // Shuffle deck
 
 function shuffle() {
-  // return hands before shuffling?
-  // pHand.forEach(function(){
-  //     deck.push(pHand.pop())
-  // })
-  // dHand.forEach(function(){
-  //     deck.push(dHand.pop())
-  // })
+	// return hands before shuffling?
+	// pHand.forEach(function(){
+	//     deck.push(pHand.pop())
+	// })
+	// dHand.forEach(function(){
+	//     deck.push(dHand.pop())
+	// })
 
-  for (let j = 0; j < 1000; j++) {
-    for (let i = 0; i < deck.length; i++) {
-      let random = Math.floor(Math.random() * deck.length);
-      let temp = deck[i];
-      deck[i] = deck[random];
-      deck[random] = temp;
-    }
-  }
-  // console.log(deck);
+	for (let j = 0; j < 1000; j++) {
+		for (let i = 0; i < deck.length; i++) {
+			let random = Math.floor(Math.random() * deck.length);
+			let temp = deck[i];
+			deck[i] = deck[random];
+			deck[random] = temp;
+		}
+	}
+	// console.log(deck);
 }
 
 // Deal cards
 function deal(deck) {
-  pHand.push(deck.pop(), deck.pop());
-  dHand.push(deck.pop(), deck.pop());
+	pHand.push(deck.pop(), deck.pop());
+	dHand.push(deck.pop(), deck.pop());
 }
 
 //Calculate hand values
 function calcHandVal(hand) {
-  let sum = 0;
-  hand.forEach(function (card, i) {
-    if (card.value == "J" || card.value == "Q" || card.value == "K") {
-      sum += 10;
-    } else if (card.value == "A") {
-      sum + 11 <= 21 ? (sum += 11) : (sum += 1);
-    } else {
-      sum += card.value;
-    }
-  });
-  return sum;
-  
-
+	let sum = 0;
+	hand.forEach(function (card, i) {
+		if (card.value == "J" || card.value == "Q" || card.value == "K") {
+			sum += 10;
+		} else if (card.value == "A") {
+			sum + 11 <= 21 ? (sum += 11) : (sum += 1);
+		} else {
+			sum += card.value;
+		}
+	});
+	return sum;
 }
 
 shuffleBtn.addEventListener("click", function () {
-  shuffle();
+	shuffle();
 });
 
 dealBtn.addEventListener("click", init);
 
-hitBtn.addEventListener('click', hit)
-stayBtn.addEventListener('click', stay)
-
+hitBtn.addEventListener("click", hit);
+stayBtn.addEventListener("click", stay);
